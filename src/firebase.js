64b5b2563768e51192cb46d33692bedced456982
terminatorDX <jain.cj.chetan@gmail.com>
2020-03-13 16:25:18 +0530

    removed firebase key from firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,23 @@
-import firebase from "firebase/app"
-import "firebase/firestore"
+import firebase from "firebase/app";
+import "firebase/firestore";
+import {
+    apiKey,
+    authDomain,
+    databaseURL,
+    projectId,
+    storageBucket,
+    messagingSenderId,
+    appId
+} from "./keys";
 
 const firebaseConfig = firebase.initializeApp({
-    apiKey: "AIzaSyD87BCA7Dq85KT1JAZzo1rhQzuO9_ETO4k",
-    authDomain: "chetan-todolist.firebaseapp.com",
-    databaseURL: "https://chetan-todolist.firebaseio.com",
-    projectId: "chetan-todolist",
-    storageBucket: "chetan-todolist.appspot.com",
-    messagingSenderId: "89382615338",
-    appId: "1:89382615338:web:6a53c22b52c64afbfc8653"
-})
+    apiKey: apiKey,
+    authDomain: authDomain,
+    databaseURL: databaseURL,
+    projectId: projectId,
+    storageBucket: storageBucket,
+    messagingSenderId: messagingSenderId,
+    appId: appId
+});
 
-export { firebaseConfig as firebase }
+export { firebaseConfig as firebase };
--- src/hooks/index.js
+++ src/hooks/index.js
@@ -1,17 +1,21 @@
-import { useState, useEffect } from "react"
-import {  firebase} from '../firebase'
-
-export  function useTasks(selectedProject) {
-    const [tasks, setTasks] = useState([])
+import { useState, useEffect } from "react";
+import { firebase } from "../firebase";
+const collectedTasks = () => {};
+export function useTasks(selectedProject) {
+    const [tasks, setTasks] = useState([]);
     useEffect(() => {
-        effect
-        return () => {
-            cleanup
-        }
-    }, [input])
-    return (
-        <div>
-            
-        </div>
-    )
+        let unsubscribed = firebase
+            .firestore()
+            .collection("tasks")
+            .where("userId", "==", "1234567890");
+        unsubscribed =
+            selectedProject && !collatedTasksExist(selectedProject)
+                ? (unsubscribed = unsubscribed.where("projectId", "==".selectedProject))
+                : selectedProject === "Today"
+                ? (unsubscribed = unsubscribed.where("date", "==", moment().format(DD / MM / YYY)))
+                : selectedProject === "Inbox" || selectedProject === 0
+                ? (unsubscribed = unsubscribed.where("date", "==", ""))
+                : unsubscribed;
+    }, [selectedProject]);
+    return <div></div>;
 }
